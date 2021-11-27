58d8baa4dca4c90cd0e029576cfa51ee05b512a8
GeForceNero17 <77616414+GeForceNero17@users.noreply.github.com>
2021-11-27 14:43:21 +0200

    Create TransparentBlt.cpp
    Simple TransparentBlt GDI Effect

---
+++ TransparentBlt.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <cmath>
+#pragma comment(lib, "Msimg32.lib")
+
+DWORD WINAPI TransparentBltExample(LPVOID Example)
+{
+    HDC hdcScreen = GetDC(0);
+    int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+    COLORREF TransparentBltColor = (RGB(rand() % 255, rand() % 255, rand() % 255));
+    while (1) {
+        TransparentBlt(hdcScreen, rand() % 255, rand() % 255, sw, sh, hdcScreen, rand() % 255, rand() % 255, rand() % 255, rand() % 255, TransparentBltColor);
+    }
+}
+
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR szCmdNShow, int nCmdShow)
+{
+    HANDLE TransparentBltSimpleExample = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)TransparentBltExample, 0, 0, 0);
+    Sleep(-1);
+}
